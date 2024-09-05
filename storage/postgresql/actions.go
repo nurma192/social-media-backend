f1fda682ce842a5655c3bebb964ccc2e5cda2d39
nurma192 <nurmakhanbetkenes@gmail.com>
2024-09-05 12:02:25 +0500

    added func GetUserByEmail

--- storage/postgresql/actions.go
+++ storage/postgresql/actions.go
@@ -1,11 +1,22 @@
 package postgresql
 
-import "social_media_backend/storage/models"
+import (
+	"errors"
+	"social_media_backend/storage/models"
+)
 
 func (s *Storage) IsExistByEmail(email string) bool {
 	var userFromDB models.User
 	err := s.DB.First(&userFromDB, models.User{Email: email}).Error
 
 	return err == nil && userFromDB.Email == email
+}
 
+func (s *Storage) GetUserByEmail(email string) (models.User, error) {
+	var userFromDB models.User
+	err := s.DB.First(&userFromDB, models.User{Email: email}).Error
+	if err != nil {
+		return models.User{}, errors.New("User not found")
+	}
+	return userFromDB, nil
 }
