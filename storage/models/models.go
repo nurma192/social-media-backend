c9322c14ca2f65aaba0793803daafb805a85f170
nurma192 <nurmakhanbetkenes@gmail.com>
2024-09-14 15:07:08 +0500

    models struct

--- storage/models/models.go
+++ storage/models/models.go
@@ -1,60 +1,55 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
-	ID          uint      `json:"id" gorm:"primary_key"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"CreatedAt"`
+	UpdatedAt time.Time      `json:"UpdatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
 	Email       string    `json:"email" gorm:"unique;not null"`
 	Password    string    `json:"password" gorm:"not null"`
 	Name        string    `json:"name"`
 	AvatarURL   string    `json:"avatar_url"`
 	DateOfBirth time.Time `json:"date_of_birth" gorm:"default:NULL"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
 	Bio         string    `json:"bio"`
 	Location    string    `json:"location"`
-	Posts       []Post    `json:"posts"`
-	Likes       []Like    `json:"likes"`
-	Comments    []Comment `json:"comments"`
-	Followers   []Follow  `json:"followers" gorm:"foreignKey:FollowingID;references:ID"`
-	Following   []Follow  `json:"following" gorm:"foreignKey:FollowerID;references:ID"`
-}
-
-type Follow struct {
-	ID          uint `json:"id" gorm:"primary_key"`
-	FollowerID  uint `json:"follower_id" gorm:"not null"`
-	FollowingID uint `json:"following_id" gorm:"not null"`
-	Follower    User `json:"follower" gorm:"foreignKey:FollowerID;references:ID"`
-	Following   User `json:"following" gorm:"foreignKey:FollowingID;references:ID"`
-}
-
-type Post struct {
-	ID        uint      `json:"id" gorm:"primary_key"`
-	Content   string    `json:"content" gorm:"not null"`
-	Author    User      `json:"author" gorm:"foreignKey:AuthorID;references:ID"`
-	AuthorID  uint      `json:"author_id" gorm:"not null"`
-	Likes     []Like    `json:"likes"`
-	Comments  []Comment `json:"comments"`
-	CreatedAt time.Time `json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
-	UpdatedAt time.Time `json:"updated_at"`
+	Likes       []Like    `json:"likes"      `
+	Posts       []Post    `json:"posts"      `
+	Comments    []Comment `json:"comments"   `
+	Followers   []User    `json:"followers"  gorm:"many2many:followers"`
+	Followings  []User    `json:"followings" gorm:"many2many:followings"`
 }
 
 type Like struct {
-	ID        uint      `json:"id" gorm:"primary_key"`
-	User      User      `json:"user" gorm:"foreignKey:UserID;references:ID"`
-	UserID    uint      `json:"user_id" gorm:"not null"`
-	Post      Post      `json:"post" gorm:"foreignKey:PostID;references:ID"`
-	PostID    uint      `json:"post_id" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"CreatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	UserID    uint           `json:"userID" gorm:"not null;uniqueIndex:idx_user_post"`
+	PostID    uint           `json:"postID" gorm:"not null;uniqueIndex:idx_user_post"`
 }
 
 type Comment struct {
-	ID        uint      `json:"id" gorm:"primary_key"`
-	Content   string    `json:"content" gorm:"not null"`
-	User      User      `json:"user" gorm:"foreignKey:UserID;references:ID"`
-	UserID    uint      `json:"user_id" gorm:"not null"`
-	Post      Post      `json:"post" gorm:"foreignKey:PostID;references:ID"`
-	PostID    uint      `json:"post_id" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"CreatedAt"`
+	UpdatedAt time.Time      `json:"UpdatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Content   string         `json:"content" gorm:"not null"`
+	UserID    uint           `json:"userID" gorm:"not null"`
+	PostID    uint           `json:"postID" gorm:"not null"`
+}
+
+type Post struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"CreatedAt"`
+	UpdatedAt time.Time      `json:"UpdatedAt"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Content   string         `json:"content"  gorm:"size:255;not null"`
+	UserID    uint           `json:"authorID" gorm:"not null"`
+	Likes     []Like         `json:"likes"`
+	Comments  []Comment      `json:"comments"`
 }
