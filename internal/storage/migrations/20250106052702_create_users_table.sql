1c0ac5398ef24dacb60983133e98a7910711029c
nurma192 <nurmakhanbetkenes@gmail.com>
2025-01-08 11:59:11 +0500

    update user table add verified field

--- internal/storage/migrations/20250106052702_create_users_table.sql
+++ internal/storage/migrations/20250106052702_create_users_table.sql
@@ -5,10 +5,12 @@ CREATE TABLE users
     id            SERIAL PRIMARY KEY,
     email         VARCHAR(100) NOT NULL UNIQUE,
     password      VARCHAR(255) NOT NULL,
-    name          VARCHAR(100),
+    firstname     VARCHAR(100) NOT NULL,
+    lastname      VARCHAR(100) NOT NULL,
     avatar_url    VARCHAR(255),
     date_of_birth DATE,
     bio           TEXT,
+    verified      BOOLEAN DEFAULT FALSE,
     location      VARCHAR(100),
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
